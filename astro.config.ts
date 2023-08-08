af57d8e56c6c63b434db07e48d79f5ca960a542f
arisris <aris@mail.kliksob.com>
2023-08-08 15:55:16 +0700

    Update

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from "astro/config";
 import unoCss from "unocss/astro";
 import presetUno from "unocss/preset-uno";
+import presetIcons from "unocss/preset-icons";
 
 export default defineConfig({
+  output: "static",
   integrations: [
     unoCss({
       presets: [
         presetUno({
           dark: "class"
+        }),
+        presetIcons({
+          autoInstall: false,
+          collections: {
+            heroicons: () =>
+              import("@iconify-json/heroicons/icons.json").then(
+                (i) => i.default
+              ),
+            logos: async () =>
+              await import("@iconify-json/logos/icons.json").then((i) => i.default) as Promise<any>
+          }
         })
       ]
     })
@@ -15,5 +28,8 @@ export default defineConfig({
   markdown: {
     gfm: true,
     syntaxHighlight: "prism"
+  },
+  experimental: {
+    assets: true
   }
 });
