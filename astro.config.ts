b0b5caba4f5c366ad612f676c0aaf095036db4fa
arisris <aris@mail.kliksob.com>
2023-08-18 07:47:50 +0700

    Update

--- astro.config.ts
+++ astro.config.ts
@@ -1,41 +1,17 @@
 import { defineConfig } from "astro/config";
-import unoCss from "unocss/astro";
-import presetUno from "unocss/preset-uno";
-import presetIcons from "unocss/preset-icons";
-import presetTypography from "unocss/preset-typography";
 import solid from "@astrojs/solid-js";
-import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   output: "static",
-  integrations: [
-    unoCss({
-      presets: [
-        presetUno({
-          dark: "class"
-        }),
-        presetTypography(),
-        presetIcons({
-          collections: {
-            async ri() {
-              const mod = await import("@iconify-json/ri");
-              return mod.icons;
-            },
-            async logos() {
-              const mod = await import("@iconify-json/logos");
-              return mod.icons;
-            }
-          }
-        })
-      ]
-    }),
-    solid(),
-    mdx({
-      gfm: true
-    })
-  ],
+  integrations: [tailwind(), solid(), sitemap()],
   experimental: {
+    optimizeHoistedScript: true,
     assets: true
   },
-  compressHTML: true
+  site: isProd ? "https://arisris.com" : "http://localhost:3000",
+  compressHTML: isProd
 });
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const tailwind = {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,ts,tsx}"]
+};
+
+module.exports = tailwind;
